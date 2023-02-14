b57b2e98451b58ec6292afc28dfb66c3196ea8f0
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2023-02-14 17:38:19 +0200

    update

---
+++ database/migrations/2023_02_14_153528_create_admin_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admin', function (Blueprint $table) {
+            $table->integer('id')->autoIncrement();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->string('password');
+            $table->string('admin_img');
+            $table->string('gender');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admin');
+    }
+};
